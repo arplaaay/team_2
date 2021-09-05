587a9363ca45d939268f1a76bc37d02156b0fef3
arplaaay <86619106+arplaaay@users.noreply.github.com>
2021-09-05 15:02:19 +0000

    Deploying to gh-pages from @ arplaaay/team_2@e79d813c07eb4bc1b8652fc22c4739b8bf78ab02 🚀

--- src.e85d16c6.js
+++ src.e85d16c6.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team_2/src.3530d402.js.map
+//# sourceMappingURL=/team_2/src.e85d16c6.js.map
