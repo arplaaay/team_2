4a6139361a422b0d68f41e6546d54a107971439a
Vitaliy Dianov <vitalik.dianv@gmail.com>
2021-09-06 15:59:38 +0300

    add mobile slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,37 +1,69 @@
 $(document).on('ready', function () {
   $('.js-slick').slick({
-    // dots: true,
-    // vertical: true,
-    // centerMode: true,
-    // arrows: false,
-    // infinite: true,
+    nextArrow: `<button class="review-btn slick-next" type="button">
+    <svg class="icon-arrow">
+      <use href="/icons-sprite.e07a9deb.svg#icon-arrow-right">
+     </use>
+    </svg>
+     </button>`,
+    prevArrow: `<button class="review-btn slick-prev" type="button">
+    <svg class="icon-arrow">
+      <use href="/icons-sprite.e07a9deb.svg#icon-arrow-left">
+     </use>
+    </svg>
+    </button>`,
+    mobileFirst: true,
+    asNavFor: '.js-slick2',
     slidesToShow: 1,
     slidesToScroll: 1,
-    nextArrow: `<button class="review-btn slick-next" type="button">
-      <svg class="icon-arrow">
-        <use href="/icons-sprite.e07a9deb.svg#icon-arrow-right">
-    </use>
-      </svg>
-    </button>`,
-    prevArrow: `<button class="review-btn slick-prev" type="button">
-      <svg class="icon-arrow">
-        <use href="/icons-sprite.e07a9deb.svg#icon-arrow-left">
-    </use>
-      </svg>
-    </button>`,
-    asNavFor: '.js-slick2',
-  });
-  $('.js-slick2').slick({
-    //   dots: true,
-    // vertical: true,
-      centerMode: true,
     arrows:false,
-    //   infinite: true,
-    slidesToShow: 7,
-    // slidesToScroll: 1,
-    // draggable: false,
-    useTransform: false,
+    // --------------------------
+    responsive:[
+        {
+            breakpoint:766,
+            settings:{
+                arrows:true
+            }
+            
+        }
+    ],
+
+    appendArrows: $('.js-slick'),
+  });
+
+  $('.js-slick2').slick({
+    nextArrow: `<button class="review-btn arrow-pagination-next slick-next" type="button">
+    <svg class="icon-arrow">
+      <use href="/icons-sprite.e07a9deb.svg#icon-arrow-right">
+  </use>
+    </svg>
+  </button>`,
+    prevArrow: `<button class="review-btn arrow-pagination-prev slick-prev" type="button">
+    <svg class="icon-arrow">
+      <use href="/icons-sprite.e07a9deb.svg#icon-arrow-left">
+  </use>
+    </svg>
+  </button>`,
+  arrows:true,
+  slidesToShow: 3,
+    mobileFirst: true,
+    useTransform: true,
+    centerMode: true,
+    centerPadding: '0px' ,
     asNavFor: '.js-slick',
+    // appendArrows: $('.js-slick2'),
+    // ---------------------
+    responsive:[
+        {
+            breakpoint:766,
+            settings:{
+                slidesToShow: 7,
+                arrows: false,
+                useTransform: false,
+                
+            }
+        }
+    ]
   });
   // });
 });
